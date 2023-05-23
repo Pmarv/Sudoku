efe6094d47795dcae4ead5cee1265ece3101bbbd
Lore <marvinpitz@gmx.de>
2023-05-23 17:58:14 +0200

    first try connecting to server and holepunch

---
+++ src/main/java/com/fs/sudoku/Backend/Multiplayer/Client.java
@@ -0,0 +1,63 @@
+package com.fs.sudoku.Backend.Multiplayer;
+
+
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
+import java.util.UUID;
+@Component
+@NoArgsConstructor
+public class Client {
+    private Socket tcpSocket;
+    private DatagramSocket udpSocket;
+    private DatagramPacket udpPacket;
+    private BufferedReader tcpIn;
+//    private BufferedOutputStream tcpOut;
+    private String uuidString;
+    InetAddress serverIp;
+    boolean gotResponse;
+    String otherClientIP;
+    String otherClientPort;
+
+    private void sendInitialUDPPacket(String uuid,String code) {
+        String sendingString = uuid + "&&" + code;
+        byte[] sendingBytes = sendingString.getBytes();
+        udpPacket = new DatagramPacket(sendingBytes,sendingBytes.length,serverIp,5001);
+        try {
+            for(int i = 0;i < 10; i++) {
+            udpSocket.send(udpPacket);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public boolean connectToOtherClient(String code) throws IOException {
+        try {
+            serverIp = InetAddress.getByName("206.189.251.215");
+            tcpSocket = new Socket(serverIp,5000);
+            tcpIn = new BufferedReader(new InputStreamReader(tcpSocket.getInputStream()));
+            uuidString = tcpIn.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        udpSocket = new DatagramSocket();
+        sendInitialUDPPacket(uuidString,code);
+        while(!gotResponse) {
+            String response = tcpIn.readLine();
+            String[] responseParts = response.split("&&");
+            otherClientIP = responseParts[0];
+            otherClientPort = responseParts[1];
+            System.out.println(otherClientIP + otherClientPort);
+            gotResponse = true;
+        }
+        udpSocket.close();
+        tcpIn.close();
+        tcpSocket.close();
+        return true;
+    }
+}
--- src/main/java/com/fs/sudoku/Backend/SudokuService.java
+++ src/main/java/com/fs/sudoku/Backend/SudokuService.java
@@ -1,13 +1,11 @@
 package com.fs.sudoku.Backend;
 
-import org.javatuples.Pair;
+import com.fs.sudoku.Backend.Multiplayer.Client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.Scanner;
 
 @Service
 public class SudokuService implements CommandLineRunner {
@@ -17,29 +15,36 @@ public class SudokuService implements CommandLineRunner {
 
     @Autowired
     RandomPuzzleGenerator randomPuzzleGenerator;
+    @Autowired
+    Client client;
 
     Exact_Cover_solver exactCoverSolver = new Exact_Cover_solver();
 //    SudokuValidator sudokuValidator = new SudokuValidator();
 
     @Override
-    public void run(String... args) {
-        exactCoverSolver = new Exact_Cover_solver();
-        sudokuGrid.setSudokuGrid(randomPuzzleGenerator.generateRandomPuzzle());
-        Scanner input = new Scanner(System.in);
-        System.out.println("Here is your Sudoku");
-        sudokuGrid.printGrid();
-
-        while (!sudokuGrid.isComplete()) {
-            System.out.println("Enter the Row you want to edit: ");
-            int row = input.nextInt();
-            System.out.println("Enter the Column you want to edit: ");
-            int column = input.nextInt();
-            System.out.println("Enter the number you want: ");
-            int num = input.nextInt();
-            sudokuGrid.setValue(new Pair<>(row-1,column-1),num);
-            sudokuGrid.printGrid();
+    public void run(String... args)  {
+//        exactCoverSolver = new Exact_Cover_solver();
+//        sudokuGrid.setSudokuGrid(randomPuzzleGenerator.generateRandomPuzzle());
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Here is your Sudoku");
+//        sudokuGrid.printGrid();
+//
+//        while (!sudokuGrid.isComplete()) {
+//            System.out.println("Enter the Row you want to edit: ");
+//            int row = input.nextInt();
+//            System.out.println("Enter the Column you want to edit: ");
+//            int column = input.nextInt();
+//            System.out.println("Enter the number you want: ");
+//            int num = input.nextInt();
+//            sudokuGrid.setValue(new Pair<>(row-1,column-1),num);
+//            sudokuGrid.printGrid();
+//        }
+//        System.out.println("Congrats");
+        try {
+            client.connectToOtherClient("1234");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        System.out.println("Congrats");
 
     }
 }
