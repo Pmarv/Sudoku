800eb3d505b0ba9f963e88239661610c289af262
ronen7 <ronen@pamagiro.de>
2023-06-14 16:31:00 +0200

    Singleplayer fxml

--- src/main/java/com/fs/sudoku/Backend/SudokuService.java
+++ src/main/java/com/fs/sudoku/Backend/SudokuService.java
@@ -23,23 +23,7 @@ public class SudokuService implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        exactCoverSolver = new Exact_Cover_solver();
-        sudokuGrid.setSudokuGrid(randomPuzzleGenerator.generateRandomPuzzle());
-        Scanner input = new Scanner(System.in);
-        System.out.println("Here is your Sudoku");
-        sudokuGrid.printGrid();
 
-        while (!sudokuGrid.isComplete()) {
-            System.out.println("Enter the Row you want to edit: ");
-            int row = input.nextInt();
-            System.out.println("Enter the Column you want to edit: ");
-            int column = input.nextInt();
-            System.out.println("Enter the number you want: ");
-            int num = input.nextInt();
-            sudokuGrid.setValue(new Pair<>(row-1,column-1),num);
-            sudokuGrid.printGrid();
-        }
-        System.out.println("Congrats");
 
     }
 }
--- src/main/java/com/fs/sudoku/Frontend/ChartController.java
+++ src/main/java/com/fs/sudoku/Frontend/ChartController.java
@@ -1,7 +1,42 @@
 package com.fs.sudoku.Frontend;
 
 import org.springframework.stereotype.Component;
+import javafx.scene.control.Button;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+
+import java.io.IOException;
+
 
 @Component
 public class ChartController {
+    @FXML
+    private Button Singleplayer;
+
+        @FXML
+        private void initialize() {
+            Singleplayer.setOnAction(this::handleButtonAction);
+        }
+    private void handleButtonAction(ActionEvent event) {
+        try {
+            // Load the FXML file for the new scene
+            System.out.println(getClass().getResource("../../../Singeplayer.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("../../../Singeplayer.fxml"));
+
+            // Create the new scene and set it on the stage
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            // Handle the exception, e.g., show an error dialog
+            e.printStackTrace();
+        }
+    }
 }
